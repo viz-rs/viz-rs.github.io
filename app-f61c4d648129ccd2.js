6e5952500560c9d5ef2b6b9eeca0d99ba5ea9f74
Fangdun Tsai <cfddream@gmail.com>
2022-11-01 02:32:41 +0800

    chore(zh-ch): requests & responses

--- app-f61c4d648129ccd2.js
+++ app-f61c4d648129ccd2.js
@@ -1117,7 +1117,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('app-5fefb873b34e8627_bg.wasm', import.meta.url);
+        input = new URL('app-f61c4d648129ccd2_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
