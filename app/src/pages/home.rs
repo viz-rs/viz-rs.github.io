9f5b07e23e73cd1bb33512f5f26339ba8a2a93c9
Fangdun Tsai <cfddream@gmail.com>
2023-04-30 05:20:00 +0800

    fix(app): missing A

--- app/src/pages/home.rs
+++ app/src/pages/home.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos_router::A;
 
 #[component]
 pub fn Home(cx: Scope, version: Signal<String>) -> impl IntoView {
@@ -20,7 +21,7 @@ pub fn Home(cx: Scope, version: Signal<String>) -> impl IntoView {
                     <p class="text-4 sm:text-5 mt-4.5 mb-7.5 sm:mt-6 sm:mb-8 op-61.8 font-light">
                         <strong class="font-normal">"Viz"</strong>" 构建在 "<a href="https://tokio.rs/" target="_bank" class="text-yellow-600 font-normal">"Tokio"</a>" 和 "<a href="https://hyper.rs/" target="_bank" class="text-yellow-600 font-normal">"Hyper"</a>" 之上。"
                     </p>
-                    <a href={format!("/{}/guide/introduction", version())} class="inline-block bg-neutral-900 text-neutral-100 dark:bg-neutral-100 dark:text-neutral-900 shadow py-2 px-4.5 border-rounded font-medium text-4 cursor-pointer">"快速上手"</a>
+                    <A href={format!("/{}/guide/introduction", version())} class="inline-block bg-neutral-900 text-neutral-100 dark:bg-neutral-100 dark:text-neutral-900 shadow py-2 px-4.5 border-rounded font-medium text-4 cursor-pointer">"快速上手"</A>
                 </section>
             }
         }
