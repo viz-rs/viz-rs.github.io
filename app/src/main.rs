82c1dbc73e332b086bbae142260d7d7bfbfc447a
Fangdun Tsai <cfddream@gmail.com>
2023-11-01 19:25:09 +0800

    fix(app): set log level by debug mode

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,7 +2,11 @@ use app::*;
 use leptos::*;
 
 pub fn main() {
-    _ = console_log::init_with_level(log::Level::Debug);
+    _ = console_log::init_with_level(if cfg!(debug_assertions) {
+        log::Level::Debug
+    } else {
+        log::Level::Info
+    });
     console_error_panic_hook::set_once();
     mount_to_body(|| view! { <App /> })
 }
